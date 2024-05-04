34eb70173d893812809547735a2cb68877191afc
ttsang2111 <ttsang2111@gmail.com>
2024-05-05 04:05:25 +0700

    commit

--- app/ui/actions/pagination.tsx
+++ app/ui/actions/pagination.tsx
@@ -10,10 +10,10 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: comment in this code when you get to this point in the course
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const currentPage = Number(searchParams.get('page')) || 1;
+  const currentPage = Number(searchParams!.get('page')) || 1;
 
   const createPageURL = (pageNumber: number | string) => {
-    const params = new URLSearchParams(searchParams);
+    const params = new URLSearchParams(searchParams!);
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,7 +12,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term: string) => {
     console.log(`Searching...${term}`);
 
-    const params = new URLSearchParams(searchParams);
+    const params = new URLSearchParams(searchParams!);
     params.set('page', '1');
     if (term) {
       params.set('query', term);
@@ -32,7 +32,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams!.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
